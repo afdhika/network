714332bdd6eb6d8bdcd1dc804dc920b65d185ab7
dhikaxzz <afdhikasyahputra@gmail.com>
2026-01-14 11:08:04 +0700

    Professional CLI

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from ping_checker import ping_host
 from port_checker import check_port
 from logger import log_result
+from colorama import Fore, Style, init
+
+init(autoreset=True)
 
 PORTS = [80, 3306]
 
@@ -11,26 +14,30 @@ def load_hosts():
 def main():
     hosts = load_hosts()
 
-    print("=== Network Monitoring Tool ===\n")
+    print(Fore.CYAN + "=== Network Monitoring Tool ===\n")
 
     for host in hosts:
         status, time_ms = ping_host(host)
 
         if status:
             line = f"[UP] {host} ({time_ms} ms)"
-            print(line)
+            print(Fore.GREEN + line)
             log_result(line)
 
             for port in PORTS:
-                port_status = "OPEN" if check_port(host, port) else "CLOSED"
-                port_line = f"Port {port}: {port_status}"
-                print(f"   └─ {port_line}")
+                port_status = check_port(host, port)
+                if port_status:
+                    port_line = f"Port {port}: OPEN"
+                    print(Fore.YELLOW + f"   └─ {port_line}")
+                else:
+                    port_line = f"Port {port}: CLOSED"
+                    print(Fore.RED + f"   └─ {port_line}")
+
                 log_result(f"{host} - {port_line}")
         else:
             line = f"[DOWN] {host}"
-            print(line)
+            print(Fore.RED + line)
             log_result(line)
-
 
         print()
 
